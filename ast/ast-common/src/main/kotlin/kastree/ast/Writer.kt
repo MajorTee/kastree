d23038570d64afdc00f6647bfb896217d3feb9ed
Tatiana Gerth <tatiana.gerth@ergon.ch>
2020-07-23 11:43:52 +0200

    fix

--- ast/ast-common/src/main/kotlin/kastree/ast/Writer.kt
+++ ast/ast-common/src/main/kotlin/kastree/ast/Writer.kt
@@ -381,7 +381,7 @@ open class Writer(
 					if (args.isNotEmpty() || lambda == null) parenChildren(args)
 					if (lambda != null) append(' ').also { children(lambda) }
 					if(parent is Node.Expr.BinaryOp){
-						val nodeParent = (BinaryOp) parent
+						val nodeParent = (Node.Expr.BinaryOp) parent
 						if(nodeParent.getRhs() is Node.Expr.BinaryOp){
 							append("\n        ").lineBegin()
 						}
